1d9760f2d8d66a196df6d32a2b25b0850aaedbd5
Przemek <przemyslawdopart@gmail.com>
2020-11-06 17:38:21 +0100

    first commit

---
+++ src/main/java/com/example/inqoo/springbootcamp/library1/library1/Library1Application.java
@@ -0,0 +1,13 @@
+package com.example.inqoo.springbootcamp.library1.library1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inqoo/springbootcamp/library1/library1/Library1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inqoo.springbootcamp.library1.library1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Library1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
